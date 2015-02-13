5ecea6546d99795358547566b1509cb51228d991
niekBr <niek.brekelmans@gmail.com>
2015-02-13 22:12:16 +0100

    13-2 Niek

--- Game/src/ParaKoopaTroopa.java
+++ Game/src/ParaKoopaTroopa.java
@@ -4,6 +4,5 @@ public class ParaKoopaTroopa extends Enemy {
 	
 	public ParaKoopaTroopa(BufferedImage image, int xBegin, int yBegin, int b, int h) {
 		super(image, xBegin, yBegin, b, h, 0, 1);
-		this.vy = 0;
 	}
 }
